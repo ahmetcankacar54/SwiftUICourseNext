21b9ccb607598062c2d6eacef169675a8bd13300
Ahmet Can Kacar <ahmetcankacar@hotmail.com>
2024-03-22 17:39:28 +0300

    Initial Commit

---
+++ IntemediateCourse/IntemediateCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntemediateCourseApp.swift
+//  IntemediateCourse
+//
+//  Created by Ahmet Kaçar on 22.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct IntemediateCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
